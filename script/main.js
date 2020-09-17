7a67c0d829f4e89f732d6e989bb17a94a0a09c3d
pl.hancharou@gmail.com <pl.hancharou@gmail.com>
2020-09-17 13:23:17 +0300

    created: stories section/responsive/slider

--- script/main.js
+++ script/main.js
@@ -1,37 +1,38 @@
-var testimonialSwiper = new Swiper('.testimonial-swiper', {
-  loop: true,
-  speed: 300,
-  autoplay: {
-    delay: 5000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true
-  },
-  on: {
-    init() {
-      this.el.addEventListener('mouseenter', () => {
-        this.autoplay.stop();
-      });
-      this.el.addEventListener('mouseleave', () => {
-        this.autoplay.start();
-      });
-    }
-  },
-});
-
-var storiesSwiper = new Swiper('.stories-swiper', {
-  loop: true,
-  speed: 500,
-  spaceBetween: 18,
-  slidesPerView: 2,
-  navigation: {
-    nextEl: '.stories-button-next',
-    prevEl: '.stories-button-prev',
-  },
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
+$(document).ready(function () {
+  var testimonialSwiper = new Swiper('.testimonial-swiper', {
+    loop: true,
+    speed: 300,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    on: {
+      init() {
+        this.el.addEventListener('mouseenter', () => {
+          this.autoplay.stop();
+        });
+        this.el.addEventListener('mouseleave', () => {
+          this.autoplay.start();
+        });
+      }
+    },
+  });
+  var storiesSwiper = new Swiper('.stories-swiper', {
+    loop: true,
+    speed: 500,
+    slidesPerView: 1,
+    spaceBetween: 1,
+    navigation: {
+      nextEl: '.stories-button-next',
+      prevEl: '.stories-button-prev',
+    },
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+  });
 });

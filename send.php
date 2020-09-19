570f9742e298cdad9e0d6608cf5c0067268c9618
pl.hancharou@gmail.com <pl.hancharou@gmail.com>
2020-09-19 14:01:25 +0300

    added: php mailer

---
+++ send.php
@@ -0,0 +1,52 @@
+<?php
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$email = $_POST['email'];
+
+// Формирование самого письма
+$title = "New subscribe on Fashion";
+$body = "
+<h2>New message</h2>
+<b>E-mail:</b> $email<br>
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    //$mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'besttour.plan@gmail.com'; // Логин на почте
+    $mail->Password   = 'B3fI9jd1'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('besttour.plan@gmail.com', 'Admin Fashion'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('pl.hancharou@gmail.com');
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";}
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: subscription.html');
